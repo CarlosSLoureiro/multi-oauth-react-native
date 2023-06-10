690c1e753428debfdb1270f77b55afccbd4cf436
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-10 19:25:25 -0300

    feat: sobre view caso keyboard esteja visível

--- src/components/BaseScreen/index.tsx
+++ src/components/BaseScreen/index.tsx
@@ -1,12 +1,20 @@
-import { ScrollView, View } from "native-base";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useColorMode,View } from "native-base";
 
 import { BaseScreenProps } from './types';
-import { outerViewProps,viewProps } from './styles';
+import { outerViewProps, viewProps } from './styles';
 
-export default function BaseScreen ({ scrollable = false, style = {}, children }: BaseScreenProps) {
-  if (scrollable) {
-    return (<View {...outerViewProps}><ScrollView {...viewProps} style={style}>{ children }</ScrollView></View>);
-  } else {
-    return (<View {...outerViewProps}><View {...viewProps} style={style}>{ children }</View></View>);
-  }
+export default function BaseScreen({ style = {}, children }: BaseScreenProps) {
+  const { colorMode } = useColorMode();
+
+  return (
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flexGrow: 1, justifyContent: `center` }}
+      style={{backgroundColor: String(colorMode === `light` ? outerViewProps._light?.bg : outerViewProps._dark?.bg)}}
+      enableOnAndroid
+      enableAutomaticScroll
+    >
+      <View {...outerViewProps}><View {...viewProps} style={style}>{ children }</View></View>
+    </KeyboardAwareScrollView>
+  );
 }
--- src/components/BaseScreen/types.ts
+++ src/components/BaseScreen/types.ts
@@ -2,7 +2,6 @@ import { ReactElement } from "react";
 import { StyleProp, ViewStyle } from "react-native";
 
 export interface BaseScreenProps {
-  scrollable?: boolean;
   style?: StyleProp<ViewStyle>;
   children: ReactElement | ReactElement[];
 }
