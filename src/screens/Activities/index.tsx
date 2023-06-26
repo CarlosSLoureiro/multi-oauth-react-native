fca4ee1371b7520f32b050a463963502c1bb1ebe
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-26 01:04:43 -0300

    feat: lista user activities na tela

--- src/screens/Activities/index.tsx
+++ src/screens/Activities/index.tsx
@@ -14,23 +14,38 @@ export default function ActivitiesScreen() {
   const [ isLoading, setIsLoading ] = useState(true);
   const [ page, setPage ] = useState<number>();
   const [ list, setList ] = useState<Array<ResponseActivityModel>>([]);
+  const [ isFullList, setIsFullList ] = useState(false);
 
   const loadActivitiesPage = async (remotePage: number) => {
     try {
       if (!user) return;
 
+      setIsLoading(true);
+
       const response = await ListActivitiesRequest(user.token, remotePage);
 
-      if(response.length > 0) {
-        setList([
-          ...list,
-          ...response.filter(loadedActivity => list.every(activity => loadedActivity.id !== activity.id))
-        ]);
+      if (!response.error) {
+        if(response.length > 0) {
+          setList([
+            ...list,
+            ...response.filter(loadedActivity => list.every(activity => loadedActivity.id !== activity.id))
+          ]);
+        } else {
+          setIsFullList(true);
+        }
+      }  else {
+        addAlert({ status: `error`, message: response.message ? `${response.error}: ${response?.message}` : response.error }, 5000);
       }
     } catch (error: any) {
       addAlert({ status: `error`, message: error.message });
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const onScrollToEnd = () => {
+    if (!isFullList && !isLoading && page !== undefined) {
+      setPage(page + 1);
     }
   };
 
@@ -49,20 +64,36 @@ export default function ActivitiesScreen() {
     }
   }, [currentScreen]);
 
-  return (
-    <BaseScreen enableScroll>
-      { isLoading ? (
+  const renderList = () => {
+    if (isLoading && list.length <= 0) {
+      return (
         <>
           <ActivityCardSkeleton/>
           <ActivityCardSkeleton opacity={0.75}/>
           <ActivityCardSkeleton opacity={0.50}/>
           <ActivityCardSkeleton opacity={0.25}/>
         </>
-      ) : (
-        list.map(activity => (
-          <ActivityCard key={activity.id} activity={activity} />
-        ))
-      )}
+      );
+    } else {
+      const activityCards = list.map(activity => (
+        <ActivityCard key={activity.id} activity={activity} />
+      ));
+      if (isLoading) {
+        return [
+          ...activityCards,
+          ...[
+            <ActivityCardSkeleton key="skeleton-1" />,
+            <ActivityCardSkeleton key="skeleton-2" opacity={0.50}/>
+          ]
+        ];
+      } else {
+        return activityCards;
+      }
+    }
+  };
+  return (
+    <BaseScreen enableScroll onScrollToEnd={onScrollToEnd}>
+      { renderList() }
     </BaseScreen>
   );
 }
