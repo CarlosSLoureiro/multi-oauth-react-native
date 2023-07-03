378816927185d721a13caab068e63518e57177b0
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-07-02 21:53:09 -0300

    chore: remove mock desnecessário

--- jest.setup.js
+++ jest.setup.js
@@ -6,11 +6,3 @@ jest.mock(`query-string`, () => ({
   parse: jest.fn(() => `parsed-string`),
   stringify: jest.fn(() => `stringied-string`)
 }));
-
-jest.mock(`./src/utils/data-manager/encrypt`, () => {
-  return () => `encrypted-string`;
-});
-
-jest.mock(`./src/utils/data-manager/decrypt`, () => {
-  return () => `decrypted-string`;
-});
