e115bb866b5a8bc61fc102adca3494913f39e35e
Carlos Loureiro <carlos.loureiro@pebmed.com.br>
2023-07-07 21:07:01 -0300

    style: altera provider na apresentação da home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -13,11 +13,12 @@ import AndroidIcon from '@components/Icons/AndroidIcon';
 import APIIcon from '@components/Icons/APIIcon';
 import AppleIcon from '@components/Icons/AppleIcon';
 import ExpoIcon from '@components/Icons/ExpoIcon';
-import FacebookIcon from '@components/Icons/FacebookIcon';
 import GoogleIcon from '@components/Icons/GoogleIcon';
+import LinkedInIcon from '@components/Icons/LinkedInIcon';
 import NativeBaseIcon from '@components/Icons/NativeBaseIcon';
 import ReactIcon from '@components/Icons/ReactIcon';
 import TSIcon from '@components/Icons/TSIcon';
+import TwitterIcon from '@components/Icons/TwitterIcon';
 import WebIcon from '@components/Icons/WebIcon';
 
 export default function HomeScreen() {
@@ -111,16 +112,16 @@ export default function HomeScreen() {
             </HStack>
           </Card>
           <Card position={4}>
-            <Text paddingTop={2} paddingX={2} textAlign="center">OAuth support to <Text bold>Google</Text>, <Text bold>Faceook</Text> and others</Text>
+            <Text paddingTop={2} paddingX={2} textAlign="center">OAuth support to <Text bold>Google</Text>, <Text bold>Twitter</Text> and others</Text>
             <HStack space={2} flex={1} flexDirection="row" alignItems="center">
               <Tooltip label="Google">
                 <Link href="https://www.google.com/" isExternal>
                   <GoogleIcon size='21px' />
                 </Link>
               </Tooltip>
-              <Tooltip label="Facebook">
-                <Link href="https://www.facebook.com/" isExternal>
-                  <FacebookIcon size='25px' />
+              <Tooltip label="Twitter">
+                <Link href="https://twitter.com/" isExternal>
+                  <TwitterIcon size='30px' />
                 </Link>
               </Tooltip>
             </HStack>
